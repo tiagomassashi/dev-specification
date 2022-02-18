b6678a020c5ffdc6fd9dd1c88af0bd23bf408ca3
Tiago Massashi Nagata <tiago.nagata@outlook.com>
2022-02-17 21:50:51 -0300

    initial commit

---
+++ src/main/java/br/com/nagata/dev/DevSpecificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.nagata.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpecificationApplication.class, args);
+	}
+
+}
